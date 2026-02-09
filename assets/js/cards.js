120153294c10597369b4ce1d311f3b3f4099fd1b
bulert <178054492+bulert@users.noreply.github.com>
2026-02-08 21:40:51 -0800

    wait

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -2,7 +2,7 @@ const cards = [
   {
     imageUrl: "./assets/images/games/among-us.png",
     heading: "Among Us",
-    link: "https://mint-ub.github.io/games/among-us/game/"
+    link: "https://bulert.github.io/games/among-us/game/"
   },
   {
     imageUrl: "./assets/images/games/angry-birds.png",
@@ -12,137 +12,137 @@ const cards = [
     {
     imageUrl: "./assets/images/games/angry-birds-2.png",
     heading: "Angry Birds 2",
-    link: "https://mint-ub.github.io/games/angry-birds-2"
+    link: "https://bulert.github.io/games/angry-birds-2"
   },
   {
     imageUrl: "./assets/images/games/angry-birds-epic.png",
     heading: "Angry Birds Epic",
-    link: "https://mint-ub.github.io/games/angry-birds-epic"
+    link: "https://bulert.github.io/games/angry-birds-epic"
   },
   {
     imageUrl: "./assets/images/games/bad-piggies.png",
     heading: "Bad Piggies",
-    link: "https://mint-ub.github.io/games/bad-piggies"
+    link: "https://bulert.github.io/games/bad-piggies"
   },
   {
     imageUrl: "./assets/images/games/bad-time-simulator.png",
     heading: "Bad Time Simulator",
-    link: "https://mint-ub.github.io/games/bad-time-simulator"
+    link: "https://bulert.github.io/games/bad-time-simulator"
   },
   {
     imageUrl: "./assets/images/games/baldis-basics-in-education-and-learning.png",
     heading: "Baldi's Basics in Education and Learning",
-    link: "https://mint-ub.github.io/games/baldis-basics-in-education-and-learning"
+    link: "https://bulert.github.io/games/baldis-basics-in-education-and-learning"
   },
   {
     imageUrl: "./assets/images/games/bendy-and-the-ink-machine.png",
     heading: "Bendy and the Ink Machine",
-    link: "https://mint-ub.github.io/games/bendy-and-the-ink-machine"
+    link: "https://bulert.github.io/games/bendy-and-the-ink-machine"
   },
   {
     imageUrl: "./assets/images/games/bitlife.png",
     heading: "BitLife",
-    link: "https://mint-ub.github.io/games/bitlife"
+    link: "https://bulert.github.io/games/bitlife"
   },
   {
     imageUrl: "./assets/images/games/block-blast.png",
     heading: "Block Blast",
-    link: "https://mint-ub.github.io/games/block-blast"
+    link: "https://bulert.github.io/games/block-blast"
   },
   {
     imageUrl: "./assets/images/games/buckshot-roulette.png",
     heading: "Buckshot Roulette",
-    link: "https://mint-ub.github.io/games/buckshot-roulette"
+    link: "https://bulert.github.io/games/buckshot-roulette"
   },
   {
     imageUrl: "./assets/images/games/burrito-bison-launcha-libre.png",
     heading: "Burrito Bison: Launcha Libre",
-    link: "https://mint-ub.github.io/games/burrito-bison-launcha-libre"
+    link: "https://bulert.github.io/games/burrito-bison-launcha-libre"
   },
   {
     imageUrl: "./assets/images/games/city-smash.png",
     heading: "City Smash",
-    link: "https://mint-ub.github.io/games/city-smash"
+    link: "https://bulert.github.io/games/city-smash"
   },
   {
     imageUrl: "./assets/images/games/cookie-clicker.png",
     heading: "Cookie Clicker",
-    link: "https://mint-ub.github.io/games/cookie-clicker"
+    link: "https://bulert.github.io/games/cookie-clicker"
   },
   {
     imageUrl: "./assets/images/games/cut-the-rope.png",
     heading: "Cut The Rope",
-    link: "https://mint-ub.github.io/games/cut-the-rope"
+    link: "https://bulert.github.io/games/cut-the-rope"
   },
   {
     imageUrl: "./assets/images/games/doki-doki-literature-club.png",
     heading: "Doki Doki Literature Club",
-    link: "https://mint-ub.github.io/games/doki-doki-literature-club"
+    link: "https://bulert.github.io/games/doki-doki-literature-club"
   },
   {
     imageUrl: "./assets/images/games/drive-mad.png",
     heading: "Drive Mad",
-    link: "https://mint-ub.github.io/games/drive-mad"
+    link: "https://bulert.github.io/games/drive-mad"
   },
   {
     imageUrl: "./assets/images/games/five-nights-at-freddys.png",
     heading: "Five Nights at Freddy's",
-    link: "https://mint-ub.github.io/games/five-nights-at-freddys"
+    link: "https://bulert.github.io/games/five-nights-at-freddys"
   },
   {
     imageUrl: "./assets/images/games/five-nights-at-freddys-2.png",
     heading: "Five Nights at Freddy's 2",
-    link: "https://mint-ub.github.io/games/five-nights-at-freddys-2"
+    link: "https://bulert.github.io/games/five-nights-at-freddys-2"
   },
   {
     imageUrl: "./assets/images/games/five-nights-at-freddys-3.png",
     heading: "Five Nights at Freddy's 3",
-    link: "https://mint-ub.github.io/games/five-nights-at-freddys-3"
+    link: "https://bulert.github.io/games/five-nights-at-freddys-3"
   },
   {
     imageUrl: "./assets/images/games/five-nights-at-freddys-4.png",
     heading: "Five Nights at Freddy's 4",
-    link: "https://mint-ub.github.io/games/five-nights-at-freddys-4"
+    link: "https://bulert.github.io/games/five-nights-at-freddys-4"
   },
   {
     imageUrl: "./assets/images/games/gacha-club.png",
     heading: "Gacha Club",
-    link: "https://mint-ub.github.io/games/gacha-club-studio"
+    link: "https://bulert.github.io/games/gacha-club-studio"
   },
   {
     imageUrl: "./assets/images/games/gacha-life.png",
     heading: "Gacha Life",
-    link: "https://mint-ub.github.io/games/gacha-life"
+    link: "https://bulert.github.io/games/gacha-life"
   },
   {
     imageUrl: "./assets/images/games/geometry-dash.png",
     heading: "Geometry Dash",
-    link: "https://mint-ub.github.io/games/geometry-dash"
+    link: "https://bulert.github.io/games/geometry-dash"
   },
   {
     imageUrl: "./assets/images/games/geometry-dash-meltdown.png",
     heading: "Geometry Dash Meltdown",
-    link: "https://mint-ub.github.io/games/geometry-dash-meltdown"
+    link: "https://bulert.github.io/games/geometry-dash-meltdown"
   },
   {
     imageUrl: "./assets/images/games/geometry-dash-subzero.png",
     heading: "Geometry Dash Subzero",
-    link: "https://mint-ub.github.io/games/geometry-dash-subzero"
+    link: "https://bulert.github.io/games/geometry-dash-subzero"
   },
   {
     imageUrl: "./assets/images/games/geometry-dash-world.png",
     heading: "Geometry Dash World",
-    link: "https://mint-ub.github.io/games/geometry-dash-world"
+    link: "https://bulert.github.io/games/geometry-dash-world"
   },
   {
     imageUrl: "./assets/images/games/granny.png",
     heading: "Granny",
-    link: "https://mint-ub.github.io/games/granny"
+    link: "https://bulert.github.io/games/granny"
   },
   {
     imageUrl: "./assets/images/games/kindergarten.png",
     heading: "Kindergarten",
-    link: "https://mint-ub.github.io/games/kindergarten"
+    link: "https://bulert.github.io/games/kindergarten"
   },
   {
      
@@ -153,172 +153,172 @@ const cards = [
   {
     imageUrl: "./assets/images/games/minecraft.png",
     heading: "Minecraft",
-    link: "https://mint-ub.github.io/games/minecraft"
+    link: "https://bulert.github.io/games/minecraft"
   },
   {
     imageUrl: "./assets/images/games/papas-bakeria.png",
     heading: "Papa's Bakeria",
-    link: "https://mint-ub.github.io/games/papas-bakeria"
+    link: "https://bulert.github.io/games/papas-bakeria"
   },
   {
     imageUrl: "./assets/images/games/papas-burgeria.png",
     heading: "Papa's Burgeria",
-    link: "https://mint-ub.github.io/games/papas-burgeria"
+    link: "https://bulert.github.io/games/papas-burgeria"
   },
   {
     imageUrl: "./assets/images/games/papas-cheeseria.png",
     heading: "Papa's Cheeseria",
-    link: "https://mint-ub.github.io/games/papas-cheeseria"
+    link: "https://bulert.github.io/games/papas-cheeseria"
   },
   {
     imageUrl: "./assets/images/games/papas-cupcakeria.png",
     heading: "Papa's Cupcakeria",
-    link: "https://mint-ub.github.io/games/papas-cupcakeria"
+    link: "https://bulert.github.io/games/papas-cupcakeria"
   },
   {
     imageUrl: "./assets/images/games/papas-donuteria.png",
     heading: "Papa's Donuteria",
-    link: "https://mint-ub.github.io/games/papas-donuteria"
+    link: "https://bulert.github.io/games/papas-donuteria"
   },
   {
     imageUrl: "./assets/images/games/papas-freezeria.png",
     heading: "Papa's Freezeria",
-    link: "https://mint-ub.github.io/games/papas-freezeria"
+    link: "https://bulert.github.io/games/papas-freezeria"
   },
   {
     imageUrl: "./assets/images/games/papas-hot-doggeria.png",
     heading: "Papa's Hot Doggeria",
-    link: "https://mint-ub.github.io/games/papas-hot-doggeria"
+    link: "https://bulert.github.io/games/papas-hot-doggeria"
   },
   {
     imageUrl: "./assets/images/games/papas-pancakeria.png",
     heading: "Papa's Pancakeria",
-    link: "https://mint-ub.github.io/games/papas-pancakeria"
+    link: "https://bulert.github.io/games/papas-pancakeria"
   },
   {
     imageUrl: "./assets/images/games/papas-pastaria.png",
     heading: "Papa's Pastaria",
-    link: "https://mint-ub.github.io/games/papas-pastaria"
+    link: "https://bulert.github.io/games/papas-pastaria"
   },
   {
     imageUrl: "./assets/images/games/papas-pizzeria.png",
     heading: "Papa's Pizzeria",
-    link: "https://mint-ub.github.io/games/papas-pizzeria"
+    link: "https://bulert.github.io/games/papas-pizzeria"
   },
   {
     imageUrl: "./assets/images/games/papas-scooperia.png",
     heading: "Papa's Scooperia",
-    link: "https://mint-ub.github.io/games/papas-scooperia"
+    link: "https://bulert.github.io/games/papas-scooperia"
   },
   {
     imageUrl: "./assets/images/games/papas-sushiria.png",
     heading: "Papa's Sushiria",
-    link: "https://mint-ub.github.io/games/papas-sushiria"
+    link: "https://bulert.github.io/games/papas-sushiria"
   },
   {
     imageUrl: "./assets/images/games/papas-taco-mia.png",
     heading: "Papa's Taco Mia",
-    link: "https://mint-ub.github.io/games/papas-taco-mia"
+    link: "https://bulert.github.io/games/papas-taco-mia"
   },
   {
     imageUrl: "./assets/images/games/papas-wingeria.png",
     heading: "Papa's Wingeria",
-    link: "https://mint-ub.github.io/games/papas-wingeria"
+    link: "https://bulert.github.io/games/papas-wingeria"
   },
   {
     imageUrl: "./assets/images/games/peggle.png",
     heading: "Peggle",
-    link: "https://mint-ub.github.io/games/peggle"
+    link: "https://bulert.github.io/games/peggle"
   },
   {
     imageUrl: "./assets/images/games/plants-vs-zombies.png",
     heading: "Plants vs Zombies",
-    link: "https://mint-ub.github.io/games/plants-vs-zombies"
+    link: "https://bulert.github.io/games/plants-vs-zombies"
   },
   {
     imageUrl: "./assets/images/games/red-ball-4.png",
     heading: "Red Ball 4",
-    link: "https://mint-ub.github.io/games/red-ball-4"
+    link: "https://bulert.github.io/games/red-ball-4"
   },
   {
     imageUrl: "./assets/images/games/retro-bowl.png",
     heading: "Retro Bowl",
-    link: "https://mint-ub.github.io/games/retro-bowl"
+    link: "https://bulert.github.io/games/retro-bowl"
   },
   {
     imageUrl: "./assets/images/games/rolling-sky.png",
     heading: "Rolling Sky",
-    link: "https://mint-ub.github.io/games/rolling-sky"
+    link: "https://bulert.github.io/games/rolling-sky"
   },
   {
     imageUrl: "./assets/images/games/solar-smash.png",
     heading: "Solar Smash",
-    link: "https://mint-ub.github.io/games/solar-smash"
+    link: "https://bulert.github.io/games/solar-smash"
   },
   {
     imageUrl: "./assets/images/games/sonic-mania.png",
     heading: "Sonic Mania",
-    link: "https://mint-ub.github.io/games/sonic-mania"
+    link: "https://bulert.github.io/games/sonic-mania"
   },
   {
     imageUrl: "./assets/images/games/space-waves.png",
     heading: "Space Waves",
-    link: "https://mint-ub.github.io/games/space-waves"
+    link: "https://bulert.github.io/games/space-waves"
   },
   {
     imageUrl: "./assets/images/games/stickman-hook.png",
     heading: "Stickman Hook",
-    link: "https://mint-ub.github.io/games/stickman-hook"
+    link: "https://bulert.github.io/games/stickman-hook"
   },
   {
     imageUrl: "./assets/images/games/subway-surfers.png",
     heading: "Subway Surfers",
-    link: "https://mint-ub.github.io/games/subway-surfers"
+    link: "https://bulert.github.io/games/subway-surfers"
   },
   {
     imageUrl: "./assets/images/games/tag.png",
     heading: "Tag",
-    link: "https://mint-ub.github.io/games/tag"
+    link: "https://bulert.github.io/games/tag"
   },
   {
     imageUrl: "./assets/images/games/temple-run-2.png",
     heading: "Temple Run 2",
-    link: "https://mint-ub.github.io/games/temple-run-2"
+    link: "https://bulert.github.io/games/temple-run-2"
   },
   {
     imageUrl: "./assets/images/games/the-impossible-quiz.png",
     heading: "The Impossible Quiz",
-    link: "https://mint-ub.github.io/games/the-impossible-quiz"
+    link: "https://bulert.github.io/games/the-impossible-quiz"
   },
   {
     imageUrl: "./assets/images/games/the-impossible-quiz-2.png",
     heading: "The Impossible Quiz 2",
-    link: "https://mint-ub.github.io/games/the-impossible-quiz-2"
+    link: "https://bulert.github.io/games/the-impossible-quiz-2"
   },
   {
     imageUrl: "./assets/images/games/the-impossible-quiz-book.png",
     heading: "The Impossible Quiz Book",
-    link: "https://mint-ub.github.io/games/the-impossible-quiz-book"
+    link: "https://bulert.github.io/games/the-impossible-quiz-book"
   },
   {
     imageUrl: "./assets/images/games/the-impossible-quiz-book-2.png",
     heading: "The Impossible Quiz Book 2",
-    link: "https://mint-ub.github.io/games/the-impossible-quiz-book-2"
+    link: "https://bulert.github.io/games/the-impossible-quiz-book-2"
   },
   {
     imageUrl: "./assets/images/games/the-impossible-quiz-book-3.png",
     heading: "The Impossible Quiz Book 3",
-    link: "https://mint-ub.github.io/games/the-impossible-quiz-book-3"
+    link: "https://bulert.github.io/games/the-impossible-quiz-book-3"
   },
   {
     imageUrl: "./assets/images/games/tomb-of-the-mask.png",
     heading: "Tomb of the Mask",
-    link: "https://mint-ub.github.io/games/tomb-of-the-mask"
+    link: "https://bulert.github.io/games/tomb-of-the-mask"
   },
   {
     imageUrl: "./assets/images/games/zuma.png",
     heading: "Zuma",
-    link: "https://mint-ub.github.io/games/zuma"
+    link: "https://bulert.github.io/games/zuma"
   }
 ];
 
